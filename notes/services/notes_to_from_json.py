4db95c1083bbaedcf4ed84a9b4b20041953346f9
Андрей Сапунов <jokerjam@yandex.ru>
2023-02-12 21:48:49 +0300

    service classes append for develop

---
+++ notes/services/notes_to_from_json.py
@@ -0,0 +1,17 @@
+# Сохранение, чтениеи данных в json
+
+class NotesToTromJson:
+    # сохранение списка данных в json
+    #   data - данные для сохранения
+    #   file_name - файл для сохранения
+    #   при ошибке возвращает код ошибки, 0 - успех
+    def notes_to_json(self, data, file_name):
+        pass
+
+
+    # чтениеи списка заметок из файла json
+    #   file_name - файл с данными
+    #   при ошибке возвращает пустой список
+    def notes_from_json(self, file_name):
+        pass
+
